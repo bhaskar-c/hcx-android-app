800b46612e08c58ba11e5f3fc8fe0823637f966c
bhaskar <bha100710@gmail.com>
2018-10-11 21:44:05 +0530

    progress bar added

--- app/src/main/java/hcx/global/hcxglobal/MainActivity.java
+++ app/src/main/java/hcx/global/hcxglobal/MainActivity.java
@@ -1,31 +1,81 @@
 package hcx.global.hcxglobal;
 
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.Window;
 import android.webkit.CookieManager;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
-    WebView browser;
+    private Context mContext;
+    private Activity mActivity;
+
+    private RelativeLayout mRelativeLayout;
+    private WebView mWebView;
+    private ProgressBar mProgressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
-        browser = (WebView) findViewById(R.id.webview);
-        CookieManager.getInstance().setAcceptThirdPartyCookies(browser,true);
-        browser.getSettings().setLoadsImagesAutomatically(true);
-        browser.getSettings().setJavaScriptEnabled(true);
-        browser.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
-        browser.getSettings().setDomStorageEnabled(true);
-        browser.getSettings().setAllowUniversalAccessFromFileURLs(true);
-        browser.setWebViewClient(new MyBrowser());
-        browser.loadUrl("https://hcx.global");
+        mContext = getApplicationContext();
+        mActivity = MainActivity.this;
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.rl);
+        mWebView = (WebView) findViewById(R.id.web_view);
+        CookieManager.getInstance().setAcceptThirdPartyCookies(mWebView,true);
+        mWebView.getSettings().setLoadsImagesAutomatically(true);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setUserAgentString("Chrome/58.0.0.0 Mobile");
+        mWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        mWebView.getSettings().setDomStorageEnabled(true);
+        mWebView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+        mProgressBar = (ProgressBar) findViewById(R.id.pb);
+        renderWebPage("https://hcx.global");
+    }
 
+    protected void renderWebPage(String urlToRender){
+        mWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon){
+                mProgressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url){
+                // Do something when page loading finished
+                //Toast.makeText(mContext,"Page Loaded.",Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+        mWebView.setWebChromeClient(new WebChromeClient(){
+            public void onProgressChanged(WebView view, int newProgress){
+                mProgressBar.setProgress(newProgress);
+                if(newProgress == 100){
+                    mProgressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl(urlToRender);
     }
 
     @Override
@@ -33,8 +83,8 @@ public class MainActivity extends AppCompatActivity {
         if (event.getAction() == KeyEvent.ACTION_DOWN) {
             switch (keyCode) {
                 case KeyEvent.KEYCODE_BACK:
-                    if (browser.canGoBack()) {
-                        browser.goBack();
+                    if (mWebView.canGoBack()) {
+                        mWebView.goBack();
                     } else {
                         finish();
                     }
@@ -44,16 +94,6 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onKeyDown(keyCode, event);
     }
-
-
-    private class MyBrowser extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
-        }
-    }
-
 
 
 
